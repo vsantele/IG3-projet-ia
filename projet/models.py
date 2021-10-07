64e2553f818022a593ee6cfd62beff2f0f6d7e17
Joachim <etu44137@henallux.be>
2021-10-07 10:53:12 +0200

    fix Model syntaxe, __init__ and _fk
    Signed-off-by: Joachim <etu44137@henallux.be>

--- projet/models.py
+++ projet/models.py
@@ -23,7 +23,7 @@ def init_db():
     lg.warning("Database initialized !")
 
 
-class User(db.model):
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(40), nullable=False)
 
@@ -31,29 +31,22 @@ class User(db.model):
 
     game = db.relationship("Game", backref="game", lazy=True)
 
-    def __init__(self, user_id, user_name, user_password):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.user_password = user_password
+    
 
 
-class Game(db.model):
+class Game(db.Model):
     game_id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
 
-    user_id_fk1 = db.Column(db.Integer, db.ForeignKey("user_id"), nullable=False)
-    user_id_fk2 = db.Column(db.Integer, db.ForeignKey("user_id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user_id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user_id"), nullable=False)
 
     state = db.relationship("State", backref="state", lazy=True)
 
-    def __init__(self, game_id, date, user_id_fk1, user_id_fk2):
-        self.game_id = game_id
-        self.date = date
-        self.user_id_fk1 = user_id_fk1
-        self.user_id_fk2 = user_id_fk2
+    
 
 
-class State(db.model):
+class State(db.Model):
     state_id = db.Column(db.Integer, primary_key=True)
     board = db.Column(db.String(256), nullable=False)
     pos_player1_X = db.Column(db.Integer, nullable=False)
@@ -61,20 +54,6 @@ class State(db.model):
     pos_player2_X = db.Column(db.Integer, nullable=False)
     pos_player2_Y = db.Column(db.Integer, nullable=False)
 
-    game_id_fk = db.Column(db.Integer, db.ForeignKey("game_id"), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("game_id"), nullable=False)
 
-    def __init__(
-        self,
-        state_id,
-        board,
-        pos_player1_X,
-        pos_player1_Y,
-        pos_player2_X,
-        pos_player2_Y,
-    ):
-        self.state_id = state_id
-        self.board = board
-        self.pos_player1_X = pos_player1_X
-        self.pos_player1_Y = pos_player1_Y
-        self.pos_player2_X = pos_player2_X
-        self.pos_player2_Y = pos_player2_Y
+    
