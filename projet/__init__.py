847c90f38adf877182920c44231f39e24ed8a487
Victor Santelé <Vic16@hotmail.be>
2021-10-12 14:07:07 +0200

    add auth (basic) + change skeleton

--- projet/__init__.py
+++ projet/__init__.py
@@ -1,11 +1,43 @@
 from flask import Flask
-from .views import app
-from . import models
+from flask_login import LoginManager
 
-models.db.init_app(app)
+# APP SETUP
+
+app = Flask(__name__)
+app.config.from_object("config")
+
+# IMPORT BLUEPRINT AND REGISTER
+
+from .views import game_bp, auth_bp
+
+app.register_blueprint(game_bp)
+app.add_url_rule("/", endpoint="index")
+
+app.register_blueprint(auth_bp)
+
+# DB SETUP
+from .models import db, init_db
+
+db.init_app(app)
+
+# SETUP LOGIN MANAGER
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
+
+from .models import User
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# CLI COMMANDS
 
 
 @app.cli.command("init_db")
-def init_db():
+def cmd_init_db():
     """Command to initialize database with flask"""
-    models.init_db()
+    init_db()
--- projet/views.py
+++ projet/views.py
@@ -1,23 +1,33 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Blueprint, Flask, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import app
 
-app = Flask(__name__)
-app.config.from_object("config")
+from .models import db, User
+
+game_bp = Blueprint("game", __name__)
+auth_bp = Blueprint("auth", __name__)
+
+# MAIN ROUTE
 
 
-@app.route("/")
+@game_bp.route("/")
 def index():
     """Root route"""
+    # return "Hello world"
     return render_template("index.html")
 
 
-@app.route("/game", methods=["GET"])
+@game_bp.route("/game", methods=["GET"])
+@login_required
 def game_create():
     """Create game"""
     # TODO: add game creation + redirect /game/<game_id>
-    return redirect(url_for("game", game_id=42))
+    return redirect(url_for("game.game", game_id=42))
 
 
-@app.route("/game/<int:game_id>", methods=["GET", "POST"])
+@game_bp.route("/game/<int:game_id>", methods=["GET", "POST"])
+@login_required
 def game(game_id):
     """Send game view and game state + handle move
 
@@ -28,4 +38,75 @@ def game(game_id):
     if request.method == "POST":
         # TODO: handle move
         pass
-    return render_template("game.html", game_state={"game_id": game_id})
+    return render_template(
+        "game.html",
+        game_state={
+            "game_id": game_id,
+        },
+        name=current_user.name,
+    )
+
+
+# LOGIN ROUTE
+
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        remember = True if request.form.get("remember") else False
+
+        user = User.query.filter(User.email == email).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash("Please check your login details and try again.")
+            return redirect(url_for("auth.login"))
+
+        login_user(user, remember=remember)
+        return redirect(url_for("game.index"))
+
+    return render_template("login.html")
+
+
+@auth_bp.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password = request.form.get("password")
+
+        if email is None:
+            flash("Email is required")
+            return redirect(url_for("auth.signup"))
+        if name is None:
+            flash("Name is required")
+            return redirect(url_for("auth.signup"))
+        if password is None:
+            flash("Password is required")
+            return redirect(url_for("auth.signup"))
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash("Email address already exists")
+            return redirect(url_for("auth.signup"))
+
+        new_user = User(
+            email=email,
+            name=name,
+            password=generate_password_hash(password, method="pbkdf2:sha256"),
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("auth.login"))
+
+    return render_template("signup.html")
+
+
+@auth_bp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("game.index"))
