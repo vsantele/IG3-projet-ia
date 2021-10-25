d8cd2997b3c587dc0af35101f57148947fb023f4
Victor Santelé <Vic16@hotmail.be>
2021-10-25 11:10:26 +0200

    dumb AI

--- projet/ai.py
+++ projet/ai.py
@@ -1,3 +1,6 @@
+import random
+
+
 class AI:
     """AI Class"""
 
@@ -6,4 +9,15 @@ class AI:
 
     def get_move(self, game_state):
         """From game_state return the best move"""
-        pass
+        x = game_state.pos_player2_X
+        y = game_state.pos_player2_Y
+        choices = []
+        if x > 0:
+            choices.append("left")
+        if x < 4:
+            choices.append("right")
+        if y > 0:
+            choices.append("up")
+        if y < 4:
+            choices.append("down")
+        return random.choice(choices)
