d58ee52332e52e7ca50a2f23ccce9f6d27e3a9cf
Victor Santelé <Vic16@hotmail.be>
2021-12-10 15:20:07 +0100

    change default location

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data/app.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "DEV-t0_R3Pl4C5"
