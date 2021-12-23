3e0949e9abc874eff87519a11312fc0a065f18b7
Victor Santelé <Vic16@hotmail.be>
2021-12-23 21:54:02 +0100

    fix disconnection

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data/app.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENGINE_OPTIONS = {"pool_recycle": 3600}
 SECRET_KEY = "DEV-t0_R3Pl4C5"
